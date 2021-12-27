ec711917ae61dbf2750bee2f6d85f6007cbb999b
Mohammedwm <mmm2012012@hotmail.com>
2021-12-27 19:50:37 +0200

    m

--- app/Http/Livewire/User/UserReviewComponent.php
+++ app/Http/Livewire/User/UserReviewComponent.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Livewire\User;
 
+use App\Models\Order;
+use App\Models\OrderItem;
 use Livewire\Component;
 
 class UserReviewComponent extends Component
 {
+    public $order_item_id;
+    public $rating;
+    public $comment;
+
+    public function mount($order_item_id)
+    {
+        $this->order_item_id = $order_item_id;
+    }
+    public function addReview()
+    {
+        $this->validate([
+
+        ]);
+    }
     public function render()
     {
-        return view('livewire.user.user-review-component');
+        $orderItem = OrderItem::find($this->order_item_id);
+        return view('livewire.user.user-review-component',['orderItem'=>$orderItem])->layout('layouts.base');
     }
 }
